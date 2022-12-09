66a2cbddb45a49e4fc9b5be4564f4f8e0700c32d
bilalaymak <bilalaymak@gmail.com>
2022-12-09 22:26:16 +0300

    güncelleme

---
+++ src/main/java/day08nesteddifternary/NestedIf01.java
@@ -0,0 +1,53 @@
+package day08nesteddifternary;
+
+import java.util.Scanner;
+
+public class NestedIf01 {
+    public static void main(String[] args) {
+/*
+        Example 1: Kullanicidan 3 tane pozitif bir tamsayi aliniz.
+                Bu 3 sayinin ucgen olusturma durumunu kontrol ediniz
+        Eger ucgen ise "eskenar"
+        Ucgen olma durumunu
+        kontrol ediniz.
+
+        INFO :
+        Ucgen Olma Sarti: herhangi iki kenar toplami ucuncu kenarda buyuk olmali,
+        herhangi iki kenar farki ucuncu kenardan kucuk olmali
+        a+b>c>a-b
+        a+c>b>a-c
+        b+c>a>b-c
+        a=b=c ise eskenar ucgen
+*/
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ucgenin kenarlari icin 3 tane uzunluk giriniz");
+        /*double a = Math.abs(input.nextDouble());
+        double b = Math.abs(input.nextDouble());
+        double c = Math.abs(input.nextDouble());
+        *///Math.abs mutlak deger yapar
+
+
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double c = input.nextDouble();
+        //Kullanicinin negatif sayi girmesini affetmiyorum
+        if(a<=0 || b<=0 || c<=0){
+
+            System.out.println("Ucgenin kenarlari negatif olamaz");
+
+        }
+        if ((a + b > c && c > Math.abs(a - b)) && (a + c > b && b > Math.abs(a - c)) && (b + c > a && a > Math.abs(b - c))) {
+
+
+            if (a == b && b == c && a == c) {
+                System.out.println("Ucgen hem de eskenar ucgen...");
+            } else {
+                System.out.println("Ucgen ama eskenar degil...");
+            }
+        } else {
+            System.out.println("Sen ucgen degilsin...");
+        }
+    }
+    }
+
+
