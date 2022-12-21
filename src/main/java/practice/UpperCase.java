2f94e23912be016769b4b64d1da922ac0988f8a6
bilalaymak <bilalaymak@gmail.com>
2022-12-22 01:30:23 +0300

    güncelleme

--- src/main/java/practice/UpperCase.java
+++ src/main/java/practice/UpperCase.java
@@ -1,5 +1,7 @@
 package practice;
 
+import java.util.Arrays;
+
 public class UpperCase {
     public static void main(String[] args) {
 
@@ -21,6 +23,11 @@ public class UpperCase {
 
 
          */
+
+        int arr1[] = {1, 2, 3};
+        int arr2[] = {1, 2, 3};
+
+        System.out.println(Arrays.equals(arr1, arr2));// true
 
 
 
