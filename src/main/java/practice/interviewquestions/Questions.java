c3e13f8061f6218ffee5ab102fff6b90e0970972
bilalaymak <bilalaymak@gmail.com>
2023-01-03 17:19:48 +0300

    güncelleme

--- src/main/java/practice/interviewquestions/Questions.java
+++ src/main/java/practice/interviewquestions/Questions.java
@@ -4,29 +4,32 @@ import java.util.Scanner;
 
 public class Questions {
     //1) Write a Java Program to reverse a string
-    //2) //Write a Java Program to swap two numbers
+    //2) Write a Java Program to swap two numbers
+    //3) How many fibonacci number do you want to see?
 
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter 2 numbers to swap");
-        double n1 = scan.nextDouble();
-        double n2 = scan.nextDouble();
+        System.out.println("How many fibonacci number do you want to see?");
+        int number = scan.nextInt();
+        fibonacci(number);
+        scan.close();
+    }
 
-        System.out.println("Before swapping: " + n1 + " - " + n2);
+    public static void fibonacci(int number) {
+        int fibo1 = 1;
+        int fibo2 = 1;
+        int fibonacci = 0;
+        System.out.print(fibo1 + " ");
+        System.out.print(fibo2 + " ");
 
-        //1.Way: Use 3rd variable
-        double temp = n1;
-        n1 = n2;
-        n2 = temp;
-        System.out.println("After swapping: " + n1 + " - " + n2);
+        for (int i = 1; i < number-1; i++) {
+            fibonacci = fibo1 + fibo2;
+            fibo1 = fibo2;
+            fibo2 = fibonacci;
+            System.out.print(fibonacci + " ");
+        }
 
-        //2.Way: Do not use 3rd variable
-        n1 = n1 + n2;
-        n2 = n1 - n2;
-        n1 = n1 - n2;
-        System.out.println("After swapping: " + n1 + " - " + n2);
-
-            }
+    }
 
 }
