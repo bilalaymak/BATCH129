1ffafbe9c9930c34500458fdb271f98aec44ddc6
bilalaymak <bilalaymak@gmail.com>
2023-01-03 21:32:09 +0300

    güncelleme

--- src/main/java/practice/interviewquestions/Questions.java
+++ src/main/java/practice/interviewquestions/Questions.java
@@ -6,30 +6,41 @@ public class Questions {
     //1) Write a Java Program to reverse a string
     //2) Write a Java Program to swap two numbers
     //3) How many fibonacci number do you want to see?
+    //4) Enter a positive integer to check if it is prime or not
 
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("How many fibonacci number do you want to see?");
+        System.out.println("Enter a positive integer to check if it is prime or not");
         int number = scan.nextInt();
-        fibonacci(number);
+        primeCalculator(number);
         scan.close();
+
     }
 
-    public static void fibonacci(int number) {
-        int fibo1 = 1;
-        int fibo2 = 1;
-        int fibonacci = 0;
-        System.out.print(fibo1 + " ");
-        System.out.print(fibo2 + " ");
-
-        for (int i = 1; i < number-1; i++) {
-            fibonacci = fibo1 + fibo2;
-            fibo1 = fibo2;
-            fibo2 = fibonacci;
-            System.out.print(fibonacci + " ");
+    public static void primeCalculator(int number) {
+        int count = 0;
+        if(number>0) {
+            if (number == 1) {
+                System.out.println(number + " is prime number");
+            }else {
+                for(int i=2; i<number; i++) {
+                    if(number%i==0) {
+                        count++;
+                    }
+                }
+                if(count==0) {
+                    System.out.println(number + " is prime");
+                }else {
+                    System.out.println(number + " is not prime");
+                }
+            }
+        }else {
+            System.out.println("Enter a positive integer");
         }
 
+
     }
+
 
 }
