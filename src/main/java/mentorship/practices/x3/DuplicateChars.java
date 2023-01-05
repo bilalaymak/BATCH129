733a6848860cf7cf67891c95c96ab7b8f9c5a167
bilalaymak <bilalaymak@gmail.com>
2023-01-05 21:32:14 +0300

    güncelleme

---
+++ src/main/java/mentorship/practices/x3/DuplicateChars.java
@@ -0,0 +1,12 @@
+package mentorship.practices.x3;
+
+public class DuplicateChars {
+    public static void main(String[] args) {
+
+        //write a code that returns the duplicate chars in a string array(interview question)
+
+
+
+
+    }
+}
---
+++ src/main/java/mentorship/practices/x3/ReverseNumber.java
@@ -0,0 +1,12 @@
+package mentorship.practices.x3;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+
+        //write a program to reverse the number which user entered
+        //input: 1238
+        //output: reverse of the number : 8321
+
+
+    }
+}
