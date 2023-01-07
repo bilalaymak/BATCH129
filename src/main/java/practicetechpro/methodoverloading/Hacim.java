6192dcc65c06a0d8eab40526906e4b19b1a1b132
bilalaymak <bilalaymak@gmail.com>
2023-01-07 20:31:56 +0300

    güncelleme

--- src/main/java/practicetechpro/methodoverloading/Hacim.java
+++ src/main/java/practicetechpro/methodoverloading/Hacim.java
@@ -6,6 +6,18 @@ public class Hacim {
              //Küp, kare prizma ve dikdörtgen prizmanın hacmini hesaplayan bir code yaziniz.
              //(Method overloading kullanınız)
 
+        HacimMethods obj = new HacimMethods();
+        int kupunHacmi = obj.hacimHesapla(5);
+        System.out.println("kupunHacmi = " + kupunHacmi);
+
+        int karePrizmaninHacmi = obj.hacimHesapla(5,4);
+        System.out.println("karePrizmaninHacmi = " + karePrizmaninHacmi);
+
+        int dikdortgenPrizmaninHacmi = obj.hacimHesapla(2,3,4);
+        System.out.println("dikdortgenPrizmaninHacmi = " + dikdortgenPrizmaninHacmi);
+
+        int kupunHacmi2 = obj.hacimHesapla(2);
+        System.out.println("kupunHacmi2 = " + kupunHacmi2);
 
 
 
--- src/main/java/practicetechpro/methodoverloading/HacimMethods.java
+++ src/main/java/practicetechpro/methodoverloading/HacimMethods.java
@@ -3,6 +3,15 @@ package practicetechpro.methodoverloading;
 public class HacimMethods {
 
 
+    public int hacimHesapla(int a){
+        return a*a*a;
+    }
+    public int hacimHesapla(int a, int b){
+        return a*a*b;
+    }
+    public int hacimHesapla(int a, int b, int c){
+        return a*b*c;
+    }
 
 
 
