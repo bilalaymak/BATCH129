8a061418b47e70b14327eddd2f3643cd04ed04fd
bilalaymak <bilalaymak@gmail.com>
2023-01-10 23:12:48 +0300

    güncelleme

--- src/main/java/practicetechpro/random/TasKagitMakas.java
+++ src/main/java/practicetechpro/random/TasKagitMakas.java
@@ -19,43 +19,86 @@ public class TasKagitMakas {
         int bilgisayarinKazanmaSayisi = 0;
         int sayac = 0;
 
-        while (oyuncununKazanmaSayisi<3 && bilgisayarinKazanmaSayisi<3){
-            sayac++;
-            System.out.println(sayac + ". secimi giriniz: \nTas \nKagit \nMakas");
-            String oyuncuTahmini = input.next().toLowerCase().trim();
-            if (!(oyuncuTahmini.equalsIgnoreCase("tas") ||
-                oyuncuTahmini.equalsIgnoreCase("kagit") ||
-                oyuncuTahmini.equalsIgnoreCase("makas" )
-            )){
-                System.out.println("gecerli bir tahmin giriniz");
-                sayac--;
-                continue;
+        while (true){
+
+            while (oyuncununKazanmaSayisi<3 && bilgisayarinKazanmaSayisi<3){
+                sayac++;
+                System.out.println(sayac + ". secimi giriniz: \nTas \nKagit \nMakas");
+                String oyuncuTahmini = input.next().toLowerCase().trim();
+                if (!(oyuncuTahmini.equalsIgnoreCase("tas") ||
+                        oyuncuTahmini.equalsIgnoreCase("kagit") ||
+                        oyuncuTahmini.equalsIgnoreCase("makas" )
+                )){
+                    System.out.println("gecerli bir tahmin giriniz");
+                    sayac--;
+                    continue;
+                }
+
+                //bilgisayarin tahmini
+                String arr[] = {"Tas","Kagit","Makas"};
+                int rastgeleIdx =  (int) (Math.random()*3);//0, 1, 2 verecek bize
+                String bilgisayarTahmini = arr[rastgeleIdx];
+                System.out.println("bilgisayarTahmini = " + bilgisayarTahmini);
+
+
+                if (oyuncuTahmini.equalsIgnoreCase(bilgisayarTahmini)){
+                    System.out.println("\nberabere! skor: " + oyuncununKazanmaSayisi + "==" + bilgisayarinKazanmaSayisi);
+                } else if (oyuncuTahmini.equalsIgnoreCase("tas") &&bilgisayarTahmini.equalsIgnoreCase("makas") ||
+                        oyuncuTahmini.equalsIgnoreCase("kagit") &&bilgisayarTahmini.equalsIgnoreCase("tas") ||
+                        oyuncuTahmini.equalsIgnoreCase("makas") &&bilgisayarTahmini.equalsIgnoreCase("kagit")) {
+                    oyuncununKazanmaSayisi++;
+                    System.out.println("\nkazandiniz! skor: " + oyuncununKazanmaSayisi + " == " + bilgisayarinKazanmaSayisi);
+
+                }else {
+                    bilgisayarinKazanmaSayisi++;
+                    System.out.println("\nbilgisayar kazandi! skor: " + oyuncununKazanmaSayisi + " == " + bilgisayarinKazanmaSayisi);
+
+                }
+
+
             }
 
-            //bilgisayarin tahmini
-            String arr[] = {"Tas","Kagit","Makas"};
-            int rastgeleIdx =  (int) (Math.random()*3);//0, 1, 2 verecek bize
-            String bilgisayarTahmini = arr[rastgeleIdx];
-            System.out.println("bilgisayarTahmini = " + bilgisayarTahmini);
+            System.out.println("===oyun bitti===");
 
+            String skor = "";
 
-            if (oyuncuTahmini.equalsIgnoreCase(bilgisayarTahmini)){
-                System.out.println("\nberabere! skor: " + oyuncununKazanmaSayisi + "==" + bilgisayarinKazanmaSayisi);
-            } else if (oyuncuTahmini.equalsIgnoreCase("tas") &&bilgisayarTahmini.equalsIgnoreCase("makas") ||
-                    oyuncuTahmini.equalsIgnoreCase("kagit") &&bilgisayarTahmini.equalsIgnoreCase("tas") ||
-                    oyuncuTahmini.equalsIgnoreCase("makas") &&bilgisayarTahmini.equalsIgnoreCase("kagit")) {
-                oyuncununKazanmaSayisi++;
-                System.out.println("\nkazandiniz! skor: " + oyuncununKazanmaSayisi + " == " + bilgisayarinKazanmaSayisi);
+            if (oyuncununKazanmaSayisi==3){
+                if (oyuncununKazanmaSayisi-bilgisayarinKazanmaSayisi==3){
+                    skor = "Altin";
+                } else if (oyuncununKazanmaSayisi-bilgisayarinKazanmaSayisi==2) {
+                    skor = "Gumus";
+                }else {
+                    skor = "Bronz";
+                }
+                System.out.println(skor + " skor ile kazandiniz! tebrikler!");
 
-            }else {
-                bilgisayarinKazanmaSayisi++;
-                System.out.println("\nbilgisayar kazandi! skor: " + oyuncununKazanmaSayisi + " == " + bilgisayarinKazanmaSayisi);
+            }else{
+                System.out.println("bilgisayar " + skor + " skor ile kazandi!");
+            }
 
+            System.out.println("devam etmek istiyor musunuz? ( evet/hayir");
+            String devam = input.next();
+            while (true){
+                if (devam.equalsIgnoreCase("hayir")){
+                    break;
+                } else if (!(devam.equalsIgnoreCase("evet") || devam.equalsIgnoreCase("hayir"))) {
+                    System.out.println("yanlis girdiniz tekrar giriniz? (evet/hayir)");
+                }
             }
 
 
         }
 
-        System.out.println("===oyun bitti===");
+
     }
 }
+
+
+
+
+
+
+
+
+
+
